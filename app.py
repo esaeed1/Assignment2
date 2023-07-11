b71bedb40715c4d6d52ddbae0f5972e107666c79
Edrees <edrees@wayne.edu>
2023-07-10 21:17:45 -0400

    it works now

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import json
 app = Flask(__name__)
 
 original_jokes = [
-    "What’s the smartest insect? A spelling bee!",
+    "What is the smartest insect? A spelling bee!",
     "Why did the scarecrow win a Nobel prize? Because she was outstanding in her field.",
     "What did one pickle say to the other? Dill with it.",
-    "Why can't you ever tell a joke around glass? It could crack up.",
+    "Why cant you ever tell a joke around glass? It could crack up.",
     "Why did the lamp sink? It was too light.",
     "How do billboards talk? Sign language.",
     "What did the ocean say to the shore? Nothing, it just waved!",
@@ -17,31 +17,18 @@ original_jokes = [
     "My boss told me to have a good day? So I went home."
 ]
 
-jokes = original_jokes.copy()
-
-
-# over here I am trying to not allow same jokes apper twice in same sesson
-def reset_jokes():
-    global jokes
-    jokes = original_jokes.copy()
-
 
 def get_jokes(num):
-    global jokes
+    random_jokes = random.sample(original_jokes, num)
     formatted_jokes = []
 
-    if len(jokes) < 2:
-        reset_jokes()
-
-    for i in range(num):
-        random_joke = random.choice(jokes)
-        question, answer = random_joke.split('? ')
+    for joke in random_jokes:
+        question, answer = joke.split('? ')
         formatted_joke = {
-            ".Question": question,  # had to add . in front because it goes Alphabetical Order
+            "Question": question,
             "Answer": answer
         }
         formatted_jokes.append(formatted_joke)
-        jokes.remove(random_joke)
 
     return formatted_jokes
 
@@ -51,11 +38,10 @@ def get_jokes_route():
     num = request.args.get('num', default=1, type=int)
     jokes_to_return = get_jokes(num)
 
-    response = jsonify(jokes_to_return)
-    response_data = json.dumps(response.json, indent=4)
+    response_data = json.dumps(jokes_to_return, indent=4)
 
     return response_data, 200, {'Content-Type': 'application/json; charset=utf-8'}
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000)
+    app.run(host='0.0.0.0', port=5000)
